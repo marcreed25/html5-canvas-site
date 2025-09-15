81e691fb4f0b7699192a471989f40c6430020fea
Marc Reed <marcreed7@outlook.com>
2025-09-15 15:26:02 +0100

    Set btn colour using HSL & change button colour on click

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "blue";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 class Button {
 
-    constructor(text, x, y, width, height) {
+    constructor(text, x, y, width, height, hue, saturation,
+            lightness) {
         this.text = text;
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
         ctx.textBaseline = 'top';
+        this.hue = hue;
+        this.saturation = saturation;
+        this.lightness = lightness;
     }
 
     draw() {
-        ctx.fillStyle = "pink";
+        ctx.fillStyle = `hsl(${this.hue}, ${this.saturation}, ${this.lightness})`;
         ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.fillStyle = "white";
         ctx.font = "50px Arial";
@@ -33,8 +35,18 @@ let btnWidth = 300;
 let btnHeight = 100;
 let btnX = (canvas.width / 2) - (btnWidth / 2);
 let btnY = (canvas.height / 2) - (btnHeight / 2);
-var btn = new Button('Hello world!', btnX, btnY, btnWidth, btnHeight);
-btn.draw();
+var btn = new Button('Hello world!', btnX, btnY, btnWidth, btnHeight,
+    '0', '0%', '50%'
+);
+
+function drawPage() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    btn.draw();
+}
+
+drawPage();
 
 function isCursorInsideButton(mx, my, btnX, btnY, btnWidth, btnHeight) {
     return mx >= btnX && mx <= btnX + btnWidth &&
@@ -49,6 +61,7 @@ canvas.addEventListener('mousedown', function(e) {
     console.log('y: ' + my);
 
     if (isCursorInsideButton(mx, my, btn.x, btn.y, btn.width, btn.height)) {
-        alert('Inside!');
+        btn.lightness = '26.7%';
+        drawPage();
     }
 })
